0fa1ab6cf96874c9b245875944c14c89247ec9cc
Nick Chapman <nick.chapman@digitalasset.com>
2019-12-04 11:21:10 +0000

    day4, parts 1 and 2

---
+++ app/Day4.hs
@@ -0,0 +1,44 @@
+
+module Day4 (main) where
+
+main :: IO ()
+main = do
+  let (a,b) = (272091,815432)
+  putStrLn $ "part1 = " <> show (part1 a b) -- 931
+  putStrLn $ "part2 = " <> show (part2 a b) -- 609
+
+part1 :: Int -> Int -> Int
+part1 min max = length $ do
+  [ n |
+    a <- [2..9],
+    b <- [a..9],
+    c <- [b..9],
+    d <- [c..9],
+    e <- [d..9],
+    f <- [e..9],
+    a==b || b==c || c==d || d==e || e==f,
+    let n = foldl (\acc x -> acc*10+x) 0 [a,b,c,d,e,f],
+    min<=n,
+    n<=max
+      ]
+
+part2 :: Int -> Int -> Int
+part2 min max = length $ do
+  [ n |
+    a <- [2..9],
+    b <- [a..9],
+    c <- [b..9],
+    d <- [c..9],
+    e <- [d..9],
+    f <- [e..9],
+
+              (a==b && b<c)
+    || (a<b && b==c && c<d)
+    || (b<c && c==d && d<e)
+    || (c<d && d==e && e<f)
+    || (d<e && e==f),
+
+    let n = foldl (\acc x -> acc*10+x) 0 [a,b,c,d,e,f],
+    min<=n,
+    n<=max
+      ]
--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,10 @@ import System.Environment (getArgs)
 import qualified Day1
 import qualified Day2
 import qualified Day3
+import qualified Day4
 
 main :: IO ()
 main = do
   [arg] <- getArgs
   let day :: Int = read arg
-  [Day1.main, Day2.main,Day3.main] !! (day-1)
+  [Day1.main,Day2.main,Day3.main,Day4.main] !! (day-1)
