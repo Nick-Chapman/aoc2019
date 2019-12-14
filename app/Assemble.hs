f2454a3ec9d6b1e16771b93e3e763b6aa84eeb35
Nick Chapman <nic@amadido.co.uk>
2019-12-14 02:45:13 +0000

    make working loader program

--- app/Assemble.hs
+++ app/Assemble.hs
@@ -10,31 +10,94 @@ import Control.Monad.Fix (MonadFix,mfix)
 import Prelude hiding (LT,EQ)
 import qualified IntMachine as IM
 
+----------------------------------------------------------------------
+
+loader1 :: Asm ()
+loader1 = mdo
+
+  Pos tmp <- Free
+
+  input sizeDest
+
+  let offset = 3000
+
+  Loc loopA <- here
+  copy (Pos loopB) (Pos (loopB+offset))
+  increment (Pos (loopA+2))
+  increment (Pos (loopA+3))
+  equal (Positional (Pos (loopA+2))) (Immediate end) (Pos tmp)
+  jmpZ (Pos tmp) (Loc loopA)
+  jmp (Loc (loopB+offset))
+
+  Loc loopB <- here
+  input (Pos 0)
+  increment (Pos (offset+loopB+1))
+  let sizeDest = Pos (loopB+8)
+  equal (Positional (Pos (offset+loopB+1))) (Immediate 0) (Pos (offset+tmp))
+  jmpZ (Pos (offset+tmp)) (Loc (loopB+offset))
+
+  jmp (Loc 0)
+
+  Loc end <- here
+  return ()
+
+
+----------------------------------------------------------------------
+
+loader2 :: Asm ()
+loader2 = mdo
+
+  -- One cell for temporary calculations
+  Pos tmp <- Free
+
+  -- The size of the program we shall read-in and exec
+  input sizeDest
+
+  -- The minimum offset by which Part B must be shifted (it's own size)
+  -- (in case we are reading a very small program)
+  let offset = end-loopB
+
+  -- All instruction arguments which embed the minimum offset value are updated
+  -- by increasing with the size of the program being read.
+  let ps = [loopA+3, loopB-1, loopB+4, loopB+5, loopB+7, loopB+9, loopB+11, loopB+12]
+  mapM_ (increase sizeDest . Pos) ps
+
+  -- Part A -- This loop copies the Part-B code up to higher memory
+  -- we need to shift up by at least the size of the new program being read-in
+  Loc loopA <- here
+  copy (Pos loopB) (Pos (loopB+offset))
+  increment (Pos (loopA+2))
+  increment (Pos (loopA+3))
+  equal (Positional (Pos (loopA+2))) (Immediate end) (Pos tmp)
+  jmpZ (Pos tmp) (Loc loopA)
+  jmp (Loc (loopB+offset))
+
+  -- Part B -- This loop reads the input into Pos-0 onwards
+  Loc loopB <- here
+  input (Pos 0)
+  increment (Pos (offset+loopB+1))
+  let sizeDest = Pos (loopB+8)
+  equal (Positional (Pos (offset+loopB+1))) (Immediate 0) (Pos (offset+tmp))
+  jmpZ (Pos (offset+tmp)) (Loc (loopB+offset))
+
+  -- Jump to the just read-in program
+  jmp (Loc 0)
+
+  Loc end <- here
+  return ()
+
+----------------------------------------------------------------------
 
 main :: IO ()
 main = do
-  -- _main_regs
-  main_vis
+  runTestsUsingLoader "loader1" loader1
+  runTestsUsingLoader "loader2" loader2
+  --main_vis
+  return ()
 
-_main_regs :: IO ()
-_main_regs = do
-  testA asm1 [42,13] "asm1" [110]
-  testA asm2 [4,42,13,-1,1000] "asm2" [1054]
-  testQ quine1 "quine1"
-  testQ quine2 "quine2"
-  testQ quine3 "quine3"
-  testQ quineSM "quineSM"
-  testA asm2_relocated [4,42,13,-1,1000] "asm2-relocated" [1054]
-  testA asm2_relocated2 [4,42,13,-1,1000] "asm2-relocated2" [1054]
-  testA loader (length halt' : halt' ++ []) "loader-halt" []
-  testA loader (length asm1' : asm1' ++ [42,13]) "loader-asm1" [110]
-    where
-      (IM.Prog halt') = assemble halt
-      (IM.Prog asm1') = assemble asm1
-
-
-main_vis :: IO ()
-main_vis = do
+{-
+_main_vis :: IO ()
+_main_vis = do
   putStrLn "visualize..."
   visualize asm1 [42,13]
 
@@ -43,32 +106,59 @@ visualize asm input = do
   let prog = assemble asm
   let sos  = IM.execA prog input
   mapM_ print sos
+-}
 
+runTestsUsingLoader :: String -> Asm () -> IO ()
+runTestsUsingLoader ltag loader = do
+  testA halt [] "halt" []
+  testA asm0 [42,13] "asm0" [110]
+  testA asm1 [42,13] "asm1" [110]
+  testA asm2 [4,42,13,-1,1000] "asm2" [1054]
+  testQ quine1 "quine1"
+  testQ quine2 "quine2"
+  testQ quine3 "quine3"
+  testQ quineSM "quineSM"
+  --testA asm2_relocated [4,42,13,-1,1000] "asm2-relocated" [1054]
+  --testA asm2_relocated2 [4,42,13,-1,1000] "asm2-relocated2" [1054]
+  --testA loader (length halt' : halt' ++ []) "loader-halt" []
+  --testA loader (length asm1' : asm1' ++ [42,13]) "loader-asm1" [110]
+    --where
+      --(IM.Prog halt') = assemble halt
+      --(IM.Prog asm1') = assemble asm1
+  print("----------------------------------------------------------------------")
+  let IM.Prog lc = assemble loader
+  print ("LOADER=",length lc,ltag,IM.Prog lc)
 
-testQ :: Asm () -> String -> IO ()
-testQ asm tag = do
-  let prog@(IM.Prog xoutput) = assemble asm
-  testP prog [] tag xoutput
+  where
 
-testA :: Asm () -> [Int] -> String -> [Int] -> IO ()
-testA asm = testP (assemble asm)
+    testQ :: Asm () -> String -> IO ()
+    testQ asm tag = do
+      let prog@(IM.Prog xoutput) = assemble asm
+      testP prog [] tag xoutput
 
+    testA :: Asm () -> [Int] -> String -> [Int] -> IO ()
+    testA asm = testP (assemble asm)
 
-testP :: IM.Prog -> [Int] -> String -> [Int] -> IO ()
-testP prog input tag expected = do
-  runCheckUsingLoader tag 0 prog input expected
-  --runCheckUsingLoader tag 1 prog input expected -- breaks check below
-  --runCheckUsingLoader tag 2 prog input expected
-  return ()
+    testP :: IM.Prog -> [Int] -> String -> [Int] -> IO ()
+    testP prog input tag expected = do
+      print("----------------------------------------------------------------------")
+      runCheckUsingLoader tag 0 prog input expected
+      runCheckUsingLoader tag 1 prog input expected
+      runCheckUsingLoader tag 2 prog input expected
+      --runCheckUsingLoader tag 3 prog input expected
+      return ()
+      where
+        runCheckUsingLoader :: String -> Int -> IM.Prog -> [Int] -> [Int] -> IO ()
+        runCheckUsingLoader tag n (IM.Prog prog) input expected =
+          if n <= 0 then printRunCheck tag (IM.Prog prog) input expected
+          else runCheckUsingLoader ("loader+"<>tag) (n-1) (assemble loader)
+               (length prog : prog ++ input) expected
 
-runCheckUsingLoader :: String -> Int -> IM.Prog -> [Int] -> [Int] -> IO ()
-runCheckUsingLoader tag n (IM.Prog prog) input expected =
-  if n <= 0 then printRunCheck tag (IM.Prog prog) input expected
-  else runCheckUsingLoader ("loader+"<>tag) (n-1) (assemble loader) (length prog : prog ++ input) expected
 
 printRunCheck :: String -> IM.Prog -> [Int] -> [Int] -> IO ()
 printRunCheck tag prog input expected = do
-  --print (tag, prog, input)
+  print (tag<>"/prog", prog)
+  print (tag<>"/input", input)
   let output = IM.exec prog input
   print (tag<>"/out", check output expected)
   --print (tag<>"/good", output == expected)
@@ -78,8 +168,16 @@ check :: (Eq a, Show a) => a -> a -> a
 check x y = if x == y then x else error (show (x,y))
 
 
-asm1 :: Asm () --fixed sum
-asm1 = do
+----------------------------------------------------------------------
+-- programs...
+
+asm0,asm1 :: Asm ()
+asm0 = asm1g 0
+asm1 = asm1g 100
+
+asm1g :: Int -> Asm () --fixed sum
+asm1g nopCount = do
+  sequence_ (take nopCount (repeat nop))
   let (a,b,c,d) = (Pos 100,Pos 200, Pos 300, Pos 400)
   input a
   input b
@@ -92,6 +190,7 @@ asm2 :: Asm () -- sum n-inputs, where n is given as first input
 asm2 = do
   let (n,acc,i) = (Pos 55, Pos 66, Pos 77)
   input n
+  set acc 0
   loop <- here
   input i
   addpp (acc,i) acc
@@ -145,7 +244,8 @@ quine3 = do
     jmpNZ n loop
     halt
 
-quineSM :: Asm () -- quine, using self-modifying code -- again code printed while changing make quining tricky!
+quineSM :: Asm () -- quine, using self-modifying code
+-- again code printed while changing make quining tricky!
 quineSM = do
   withSize $ \size -> mdo
     --nop
@@ -157,6 +257,7 @@ quineSM = do
     jmpNeq (Positional outputInstructionArg) (Immediate size) start
     halt
 
+{-
 asm2_relocated :: Asm ()
 asm2_relocated = mdo
   let q3 = 5000
@@ -170,54 +271,100 @@ asm2_relocated = mdo
 
 asm2_relocated2 :: Asm ()
 asm2_relocated2 = relocate_andJumpTo (Pos 5000) asm2
+-}
 
-
-
-
-loader :: Asm ()
-loader = mdo
+----------------------------------------------------------------------
+{-
+loader1 :: Asm ()
+loader1 = mdo
   input sizeDest
-  sizeDest <- relocate_andJumpTo (Pos 6000) $ do
-    loop <- here
+  let offset = 3000
+  sizeDest <- shift_andJumpToSM offset $ do
+
+    Pos tmp0 <- Free
+    let tmp = Pos (offset+tmp0)
+
+    Loc loop <- here
     input (Pos 0)
     Loc x <- here
-    let inputDest = Pos (x-1)
+    let inputDest = Pos (offset+x-1)
     increment inputDest
     Loc y <- here
     let sizeDest = Pos (y+2)
-    jmpNeq (Positional inputDest) (Immediate 567) loop
+
+    equal (Positional inputDest) (Immediate 333) tmp
+    jmpZ tmp (Loc (loop+offset))
+
     jmp (Loc 0)
     return sizeDest
   return ()
 
-
-relocate_andJumpTo :: Pos -> Asm a -> Asm a
-relocate_andJumpTo (Pos q3) asm = mdo
-  memcopy2 (Pos q1) (q2 - q1) (Pos q3)
+shift_andJumpToSM :: Int -> Asm a -> Asm a
+shift_andJumpToSM offset asm = mdo
+  let q3 = q1 + offset
+  memcopySM (Pos q1) (q2 - q1) (Pos q3)
   jmp (Loc q3)
   Loc q1 <- here
   res <- asm
   Loc q2 <- here
   return res
 
-
-----------------------------------------------------------------------
-
-memcopy2 :: Pos -> Int -> Pos -> Asm () -- using relative addresing, assuming set at 0
-memcopy2 (Pos from) len (Pos dest) = do
+memcopySM :: Pos -> Int -> Pos -> Asm () -- using self-modifying code
+memcopySM (Pos from) len (Pos dest) = do
   loop <- here
   Loc x <- here
   copy (Pos from) (Pos dest)
+  --set (Pos from) 44 -- splat data at orig location to be sure am not using it
   let (copySrc,copyDest) = (Pos (x+2), Pos (x+3))
   increment copySrc
   increment copyDest
   jmpNeq (Positional copySrc) (Immediate (from+len)) loop
   return ()
+-}
 
 
+----------------------------------------------------------------------
+{-
+loader2 :: Asm ()
+loader2 = mdo
+  input sizeDest
+  let offset = 3000
+  sizeDest <- shift_andJumpToR offset $ do
 
-memcopy :: Pos -> Int -> Pos -> Asm () -- using relative addresing, assuming set at 0
-memcopy from len dest = do
+    Pos tmp0 <- Free
+    let tmp = Pos (offset+tmp0)
+
+    Loc loop <- here
+
+    input (Pos 0)
+    Loc x <- here
+    let inputDest = Pos (offset+x-1)
+    increment inputDest
+
+    Loc y <- here
+    let sizeDest = Pos (y+2)
+
+    equal (Positional inputDest) (Immediate 333) tmp
+    jmpZ tmp (Loc (loop+offset))
+
+    jmp (Loc 0)
+    return sizeDest
+  return ()
+
+
+shift_andJumpToR :: Int -> Asm a -> Asm a
+shift_andJumpToR offset asm = mdo
+  let q3 = q1 + offset
+  memcopyR (Pos q1) len (Pos q3)
+  jmp (Loc q3)
+  let len = q2 - q1
+  Loc q1 <- here
+  res <- asm
+  Loc q2 <- here
+  return res
+
+memcopyR :: Pos -> Int -> Pos -> Asm () -- using relative addresing, assuming set at 0
+memcopyR from len dest = do
   n <- Free; set n len
   loop <- here
   copyRel from dest
@@ -227,8 +374,24 @@ memcopy from len dest = do
   adjRel (Immediate (-len))
   return ()
 
+
 copyRel :: Pos -> Pos -> Asm ()
 copyRel from dest = emit $ Instruction Add [Relative from, Immediate 0, Relative dest]
+
+-}
+
+----------------------------------------------------------------------
+
+{-relocate_andJumpTo :: Pos -> Asm a -> Asm a
+relocate_andJumpTo (Pos q3) asm = mdo
+  memcopy2 (Pos q1) (q2 - q1) (Pos q3)
+  jmp (Loc q3)
+  Loc q1 <- here
+  res <- asm
+  Loc q2 <- here
+  return res
+-}
+
 
 withSize :: (Int -> Asm a) -> Asm a
 withSize f = mdo
@@ -254,10 +417,17 @@ nop :: Asm ()
 nop = addip (0,Pos 0) (Pos 0)
 
 increment :: Pos -> Asm ()
-increment x = addip (1,x) x
+increment = increaseK 1
 
 decrement :: Pos -> Asm ()
-decrement x = addip (-1,x) x
+decrement = increaseK (-1)
+
+increaseK :: Int -> Pos -> Asm ()
+increaseK n x = addip (n,x) x
+
+increase :: Pos -> Pos -> Asm ()
+increase a x = addpp (a,x) x
+
 
 set :: Pos -> Int -> Asm ()
 set x v = addii (v,0) x
@@ -267,6 +437,7 @@ copy from dest = addip (0,from) dest
 
 ----------------------------------------------------------------------
 
+{-
 _alloc_inline :: Int -> Asm Pos
 _alloc_inline v = mdo
   jmp after
@@ -280,6 +451,7 @@ _alloc_initialized v = Later $ mdo
   Loc d <- here
   Emit [v]
   return $ Pos d
+-}
 
 jmp :: Loc -> Asm ()
 jmp (Loc dest) = emit $ Instruction JmpF [Immediate 0, Immediate dest]
