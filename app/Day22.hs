0f9bf5f1842a40fd5c113707109cc39a7bcab3f8
Nick Chapman <nic@amadido.co.uk>
2020-01-11 19:23:18 +0000

    day 22, part2, not working: repeatF is wrong

--- app/Day22.hs
+++ app/Day22.hs
@@ -1,6 +1,11 @@
 
 module Day22 (main) where
 
+--import GHC.Int
+--type Inty = Int64
+
+type Inty = Integer
+
 main :: IO ()
 main = do
   let _x1 = unlines ["deal with increment 7" ,"deal into new stack" ,"deal into new stack"]
@@ -27,17 +32,24 @@ main = do
   print (check (_xpart1 (M 10) _x4) [9,2,5,8,1,4,7,0,3,6])
 
   putStrLn $ "day22, part1 = " <> show (check (part1 s) 3324)
+  putStrLn $ "day22, part2 = " <> show (check (part2 s) 5184296506541) -- wrong answer
 
 check :: (Eq a, Show a) => a -> a -> a
 check x y = if x == y then x else error (show (x,y))
 
-part1 :: String -> Int
+part1 :: String -> Inty
 part1 str = applyF (makeF str) (M 10007) 2019
 
-_xpart1 :: M -> String -> [Int]
+_xpart1 :: M -> String -> [Inty]
 _xpart1 (M m) str = do
   let func = applyF (invertF (makeF str)) (M m)
   map func [0..m-1]
+
+part2 :: String -> Inty
+part2 str = do
+  let m = M 119315717514047
+  let r = 101741582076661
+  applyF (invertF (repeatF r (makeF str))) m 2020
 
 makeF :: String -> F
 makeF str = foldl1 seqF (map parseF (lines str))
@@ -52,67 +64,68 @@ parseF s = case words s of
 reverseF :: F
 reverseF = anyShuffle (-1) (-1)
 
-cutF :: Int -> F
+cutF :: Inty -> F
 cutF a = anyShuffle 1 (-a)
 
-dealInc :: Int -> F
+dealInc :: Inty -> F
 dealInc n = anyShuffle n 0
 
-newtype M = M Int
 
-type F = M -> F1
+newtype M = M Inty
+newtype F = F (M -> F1)
+data F1 = F1 { n :: Inty, a :: Inty }
 
-anyShuffle :: Int -> Int -> F
-anyShuffle n a = \_ -> F1 {n,a}
+anyShuffle :: Inty -> Inty -> F
+anyShuffle n a = F $ \_ -> F1 {n,a}
 
-applyF :: F -> M -> Int -> Int
-applyF f m = applyF1 (f m) m
+applyF :: F -> M -> Inty -> Inty
+applyF (F f) (M m) i = do
+  let F1{n,a} = f (M m)
+  (n * i + a) `mod` m
 
 seqF :: F -> F -> F
-seqF f g m = seqF1 m (f m) (g m)
+seqF (F f) (F g) = F $ \(M m) -> do
+  let F1{n=n1,a=a1} = f (M m)
+  let F1{n=n2,a=a2} = g (M m)
+  F1 {n = n2*n1 `mod` m, a = (n2 * a1 + a2) `mod` m}
+
+repeatF :: Inty -> F -> F -- THIS IS WRONG
+repeatF r (F f) = F $ \(M m) -> do
+  let F1{n,a} = f (M m)
+  F1{ n = r * n `mod` m, a = r * a `mod` m }
 
 invertF :: F -> F
-invertF f = \m -> invertF1 m (f m)
+invertF (F f) = F $ \(M m) -> do
+  let F1{n,a} = f (M m)
+  let n' = moduloInverseChecked (M m) n
+  F1 {n = n', a = n' * (-a) `mod` m}
 
 
-data F1 = F1 { n :: Int, a :: Int }
-
-applyF1 :: F1 -> M -> Int -> Int
-applyF1 F1{n,a} (M m) = \i -> (n * i + a) `mod` m
-
-seqF1 :: M -> F1 -> F1 -> F1  -- opposite order to normal `(.)` function composition
-seqF1 (M m) F1{n=n1,a=a1} F1{n=n2,a=a2} = F1 {n = (n2*n1) `mod` m, a = (n2 * a1 + a2) `mod` m}
-
-invertF1 :: M -> F1 -> F1
-invertF1 (M m) F1{n,a} = do
-  let n' = moduloInverse (M m) n --Checked (M m) n
-  F1 {n = n', a = n' * (-a) }
-
-_moduloInverseChecked :: M -> Int -> Int
-_moduloInverseChecked (M m) a = do
+moduloInverseChecked :: M -> Inty -> Inty
+moduloInverseChecked (M m) a = do
   let b = moduloInverse (M m) a
-  let check = (a * b) `mod` m
+  let check = a * b `mod` m
   if check == 1 then b else error $ "moduloInverse" <> show (m,a,b)
 
-moduloInverse :: M -> Int -> Int
+moduloInverse :: M -> Inty -> Inty
 moduloInverse (M m) a =
   inverseMod a m
   where
-    inverseMod :: Int -> Int -> Int
+    inverseMod :: Inty -> Inty -> Inty
     inverseMod x y = do
       let a = besout x y
       case a!!2 of
         1 -> mods (a!!0) y
         _ -> 0
 
-    besout :: Int -> Int -> [Int]
+    besout :: Inty -> Inty -> [Inty]
     besout x y = bBesout [1,0,x] [0,1,y] where
       bBesout u v =
         case v!!2 of
           0 -> u
-          _ -> let q = div (u!!2) (v!!2) in bBesout v [u!!k - q * v!!k | k <- [0..2]]
+          _ -> let q = div (u!!2) (v!!2) in
+                 bBesout v [u!!k - q * v!!k | k <- [0..2]]
 
     -- | mods return positve remainder of "mod" operator
-    mods :: Int -> Int -> Int
+    mods :: Inty -> Inty -> Inty
     mods x p = if y >= 0 then y else y + p where y = mod x p
-
