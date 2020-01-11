11020dd1d09de652fdd615f7c48cbdafce8cc4d8
Nick Chapman <nic@amadido.co.uk>
2020-01-11 11:43:33 +0000

    day 24, tidy

--- app/Day24.hs
+++ app/Day24.hs
@@ -20,7 +20,7 @@ main = do
   --_xpart2 10 example
   putStrLn $ "day24, part2 (example) = " <> show (check (part2 10 example) 99)
 
-  --_xpart2 200 full
+  _xpart2 200 full
   --putStrLn $ "day24, part2 (full) = " <> show (check (part2 200 full) 2067)
 
 check :: (Eq a, Show a) => a -> a -> a
@@ -43,40 +43,35 @@ part2 n str = do
 
 type Level = Int
 type Pos = (Level,Int,Int)
-
 newtype State = State (Set Pos) deriving (Eq,Ord)
+
+level0positions :: [Pos]
+level0positions = [ (0, x,y) | x <- [1..5], y <- [1..5] ]
 
 instance Show State where
   show (State set) = do
-    unlines $ chunksOf 5 $ map ((\b -> if b then '#' else '.') . (`elem` set)) part1positions
+    unlines $ chunksOf 5 $ map ((\b -> if b then '#' else '.') . (`elem` set)) level0positions
 
 numBugs :: State -> Int
 numBugs (State set) = Set.size set
 
-part1positions :: [Pos]
-part1positions = [ (0, x,y) | x <- [1..5], y <- [1..5] ]
-
 parse :: String -> State
 parse str = do
-  State (Set.fromList [ p | (c,p) <- zip str part1positions, decode c ])
+  State (Set.fromList [ p | (c,p) <- zip str level0positions, decode c ])
   where
     decode = \case '.' -> False; '#' -> True; c -> error $ "decode: " <> show c
 
 step :: (Pos -> [Pos]) -> State -> State
 step neighbours (State set) = State set' where
-  set' = Set.filter nextGen cand
-  cand = image neighbours set
+  set' = Set.filter nextGen $ image neighbours set
   isOn pos = pos `elem` set
-  nextGen :: Pos -> Bool
-  nextGen pos =
-    let neighboursOn = filter isOn (neighbours pos) in
-    if isOn pos
-    then length neighboursOn == 1
-    else length neighboursOn `elem` [1,2]
+  nextGen pos = do
+    let count = length $ filter isOn (neighbours pos)
+    count == 1 || not (isOn pos) && count == 2
 
 rate :: State -> Int
 rate (State set) = do
-  sum [ n | (pos,n) <- zip part1positions powers, isOn pos ]
+  sum [ n | (pos,n) <- zip level0positions powers, isOn pos ]
   where
     isOn pos = pos `elem` set
     powers = 1 : map (2*) powers
@@ -89,25 +84,21 @@ neighbours1 (_,x,y) =
 neighbours2 :: Pos -> [Pos]
 neighbours2 pos = right pos ++ left pos ++ up pos ++ down pos
   where
-    right :: Pos -> [Pos]
     right = \case
       (l,2,3) -> [(l+1,1,y) | y <- [1..5] ] -- in
       (l,5,_) -> [(l-1,4,3)] -- out
       (l,x,y) -> [(l,x+1,y)] -- stay
 
-    down :: Pos -> [Pos]
     down = \case
       (l,3,2) -> [(l+1,x,1) | x <- [1..5] ] -- in
       (l,_,5) -> [(l-1,3,4)] -- out
       (l,x,y) -> [(l,x,y+1)] -- stay
 
-    left :: Pos -> [Pos]
     left = \case
       (l,4,3) -> [(l+1,5,y) | y <- [1..5] ] -- in
       (l,1,_) -> [(l-1,2,3)] -- out
       (l,x,y) -> [(l,x-1,y)] -- stay
 
-    up :: Pos -> [Pos]
     up = \case
       (l,3,4) -> [(l+1,x,5) | x <- [1..5] ] -- in
       (l,_,1) -> [(l-1,3,2)] -- out
