6d4241f8911866479498be378e1c95c35be59741
Nick Chapman <nic@amadido.co.uk>
2020-01-11 09:46:35 +0000

    day24, redesign state representation in prep for part2

--- app/Day24.hs
+++ app/Day24.hs
@@ -2,7 +2,6 @@
 module Day24 (main) where
 
 import qualified Data.Set as Set
-import Data.List (zip5)
 import Data.List.Split (chunksOf)
 import Data.Set (Set)
 
@@ -15,8 +14,9 @@ main = do
   print s
   print (step s)
 
-  putStrLn $ "day25, part1 (example) = " <> show (check (part1 example) 2129920)
-  putStrLn $ "day25, part1 (full) = "    <> show (check (part1 full) 18859569)
+  putStrLn $ "day24, part1 (example) = " <> show (check (part1 example) 2129920)
+  putStrLn $ "day24, part1 (full) = "    <> show (check (part1 full) 18859569)
+
 check :: (Eq a, Show a) => a -> a -> a
 check x y = if x == y then x else error (show (x,y))
 
@@ -25,32 +25,46 @@ part1 str = rate $ loop Set.empty (parse str) where
   loop :: Set State -> State -> State
   loop v s = if s `elem` v then s else loop (Set.insert s v) (step s)
 
-newtype State = State [Bool] deriving (Eq,Ord)
+type Pos = (Int,Int)
+
+newtype State = State (Set Pos) deriving (Eq,Ord)
 
 instance Show State where
-  show (State s) =
-    unlines $ chunksOf 5 $ map (\b -> if b then '#' else '.') s
+  show (State set) = do
+    let ps = [ (x,y) | x <- [1..5], y <- [1..5] ]
+    unlines $ chunksOf 5 $ map ((\b -> if b then '#' else '.') . (`elem` set)) ps
 
 parse :: String -> State
-parse = State . map decode where
-  decode = \case '.' -> False; '#' -> True; c -> error $ "decode: " <> show c
+parse str = do
+  let ps = [ (x,y) | x <- [1..5], y <- [1..5] ]
+  State (Set.fromList [ p | (c,p) <- zip str ps, decode c ])
+  where
+    decode = \case '.' -> False; '#' -> True; c -> error $ "decode: " <> show c
 
 step :: State -> State
-step (State bs) = State (map stepCell (zip5 bs bs1 bs2 bs3 bs4)) where
-
-  bs1 = drop 5 bs ++ take 5 offs
-  bs2 = concat $ map (\xs -> drop 1 xs ++ [False]) (chunksOf 5 bs)
-  bs3 = concat $ map (\xs -> [False] ++ take 4 xs) (chunksOf 5 bs)
-  bs4 = take 5 offs ++ take 20 bs
-
-  offs = repeat False
-
-  stepCell (b,b1,b2,b3,b4) =
-    let neighbours = filter id [b1,b2,b3,b4] in
-    if b
-    then length neighbours == 1
-    else length neighbours `elem` [1,2]
+step (State set) = State set' where
+  set' = Set.filter nextGen cand
+  cand = image neighbours set
+  isOn pos = pos `elem` set
+  nextGen :: Pos -> Bool
+  nextGen pos =
+    let neighboursOn = filter isOn (neighbours pos) in
+    if isOn pos
+    then length neighboursOn == 1
+    else length neighboursOn `elem` [1,2]
 
 rate :: State -> Int
-rate (State bs) = sum [ n | (b,n) <- zip bs powers, b ] where
-  powers = 1 : map (2*) powers
+rate (State set) = do
+  let ps = [ (x,y) | x <- [1..5], y <- [1..5] ]
+  sum [ n | (pos,n) <- zip ps powers, isOn pos ]
+  where
+    isOn pos = pos `elem` set
+    powers = 1 : map (2*) powers
+
+neighbours :: Pos -> [Pos]
+neighbours (x,y) =
+  [ (x',y') | (x',y') <- [ (x+1,y), (x-1,y), (x,y+1), (x,y-1) ], inRange x', inRange y' ]
+  where inRange n = n >= 1 && n <= 5
+
+image :: Ord a => (a -> [a]) -> Set a -> Set a
+image f set = Set.fromList [ y | x <- Set.elems set, y <- f x ]
