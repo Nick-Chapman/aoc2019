2f7d02430ce1ef26ea783e9ea385bc67f32b0531
Nick Chapman <nic@amadido.co.uk>
2020-01-14 21:13:56 +0000

    rust, day1

---
+++ rust/src/day1.rs
@@ -0,0 +1,59 @@
+
+use std::io::BufReader;
+use std::io::BufRead;
+use std::process::exit;
+use std::fs::File;
+
+pub fn main() {
+   println!("day1, part1 = {}", check(part1(),3318632));
+   println!("day1, part2 = {}", check(part2(),4975084));
+}
+
+fn check(got : i64, expected : i64) -> i64 {
+   if got != expected {
+       println!("ensure failed: got: {}, expected: {}", got, expected);
+       exit(1)
+   }
+   got
+}
+
+fn part1() -> i64 {
+   let file = File::open("/home/nic/code/advent/input/day1.input").expect("day1 input");
+   let mut buffer = BufReader::new(file);
+   let mut acc = 0;
+   loop {
+        let mut line = String::new();
+        let n = buffer.read_line(&mut line).expect("read_line");
+        if n == 0 { break }
+        let n : i64 = line.trim().parse().expect("not an int");
+        acc += calc1(n);
+    }
+    return acc;
+}
+
+fn part2() -> i64 {
+   let file = File::open("/home/nic/code/advent/input/day1.input").expect("day1 input");
+   let mut buffer = BufReader::new(file);
+   let mut acc = 0;
+   loop {
+        let mut line = String::new();
+        let n = buffer.read_line(&mut line).expect("read_line");
+        if n == 0 { break }
+        let n : i64 = line.trim().parse().expect("not an int");
+        acc += calc2(n);
+    }
+    return acc;
+}
+
+fn calc1(x : i64) -> i64 {
+   x / 3 - 2
+}
+
+fn calc2(x : i64) -> i64 {
+   let y = calc1(x);
+   if y>0 {
+      return y + calc2(y)
+   } else {
+      return 0
+   }
+}
