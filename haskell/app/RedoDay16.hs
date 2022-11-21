de3a9007f40cee64b3c5f28fbc4df28cfc5f088a
Nick Chapman <nic@amadido.co.uk>
2022-11-21 23:27:17 +0000

    day 16, part 2... finally!

--- haskell/app/RedoDay16.hs
+++ haskell/app/RedoDay16.hs
@@ -11,16 +11,72 @@ check x y = if x == y then x else error (show (x,y))
 main :: IO ()
 main = do
   let tag = "(redo)day16"
+  s <- readFile "input/day16.input"
+
   let
     t i x = print (tag, i,"-->",check x $ part1 i)
   t "80871224585914546619083218645595" "24176176"
   t "19617804207202209144916044189917" "73745418"
   t "69317163492948606335995924319873" "52432133"
 
-  s <- readFile "input/day16.input"
   print (tag++", part1", check "19239468" $ part1 s)
 
+  let
+    t2 i x = print (tag, i,"-->",check x $ part2 i)
+
+  t2 "03036732577212944063491565474664" "84462026"
+  t2 "02935109699940807407585447034323" "78725270"
+  t2 "03081770884921959731165446850517" "53553731"
+
+  -- finally an answer for part2!!
+  print (tag++", part2", check "96966221" $ part2 s)
+
   pure ()
+
+part2 :: String -> String
+part2 s = do
+  let g0x_full = rep (init s) 10000
+  let k :: Int = read (take 7 s)
+  let
+    step :: Signal -> Signal
+    step xs = fast_step xs
+
+    steps :: Signal -> [Signal]
+    steps a = a : steps (step a)
+
+  let g0 = g0x_full
+  let gs = steps g0
+  let g100:_ = drop 100 gs
+  concat $ map show $ take 8 (drop k g100)
+
+
+fast_step :: [Dig] -> [Dig]
+fast_step xs = scanr1 (\d s -> (d + s) `mod` 10) xs
+
+{-
+_scanSum :: [Dig] -> [Dig]
+_scanSum xs0 = loop (sig xs0) xs0
+  where
+    loop :: Dig -> [Dig] -> [Dig]
+    loop z = \case
+      [] -> []
+      x:xs  -> z : loop (sub z x) xs
+
+    sub z x = (10+z-x) `mod` 10
+
+sig :: [Dig] -> Dig -- sum digits modulo 10
+sig xs =
+  sum xs `mod` 10
+-}
+
+rep :: [Dig] -> Int -> [Dig]
+rep xs = loop
+  where
+    loop = \case
+      0 -> []
+      n -> xs ++ loop (n-1)
+
+----------------------------------------------------------------------
 
 part1 :: String -> String
 part1 s = do
